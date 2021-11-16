c112428c7e1599212cf22df1bbda9fd9e6047ba6
Eric Rion <erion42@gmail.com>
2021-11-15 21:29:39 -0500

    Confirmed server.js is good to go, added notes

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
+// Basically already done for me
+// below sets up dependencies
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
+// port to listen on
 const PORT = 3000;
 
 const app = express();
@@ -15,6 +18,7 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+// Connect to the database - will need to update this for Heroku
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
   useFindAndModify: false
@@ -23,6 +27,7 @@ mongoose.connect("mongodb://localhost/budget", {
 // routes
 app.use(require("./routes/api.js"));
 
+// start listening, display port
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
 });
